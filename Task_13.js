4336474f615de16fdf54a016eb0d54ebe670352e
kishansv <kishansv079@gmail.com>
2026-08-31 19:35:59 +0530

    Create Task_13.js

---
+++ Task_13.js
@@ -0,0 +1,27 @@
+var name = "kishan";
+let age = 19;
+let isstudent = true;
+const DOB = "29/01/2007";
+
+let sum = age + 5;
+let diff = age - 5;
+let prod = age*2;
+let div = age/5;
+let remainder = age%5;
+
+let issame = (age==19);
+let isequal = (age==="19");
+
+console.log(name);
+console.log(age);
+console.log(isstudent);
+console.log(DOB);
+
+console.log(sum);
+console.log(diff);
+console.log(prod);
+console.log(div);
+console.log(remainder);
+
+console.log(issame);
+console.log(isequal);
